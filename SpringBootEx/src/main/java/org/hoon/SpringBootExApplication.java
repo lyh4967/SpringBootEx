0083c87c9c4aaf5208f65fab50d8bf9fc31b266e
lyh4967 <lyh4965@naver.com>
2018-04-06 01:53:33 +0900

    first

---
+++ SpringBootEx/src/main/java/org/hoon/SpringBootExApplication.java
@@ -0,0 +1,12 @@
+package org.hoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExApplication.class, args);
+	}
+}
