8de351d65f7e3cfc86d00668fc2fc0a967e7007b
lyh4967 <lyh4965@naver.com>
2018-04-10 02:05:20 +0900

    boot05

---
+++ SpringBootEx/src/main/java/org/hoon/domain/MemberVO.java
@@ -0,0 +1,17 @@
+package org.hoon.domain;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MemberVO {
+	
+	private int mno;
+	private String mid;
+	private String mpw;
+	private String mname;
+	private Timestamp regdate;
+}
